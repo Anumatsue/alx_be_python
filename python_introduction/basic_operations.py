170a40e8262870d40fe410eefd32f71fd19d22da
Paschal Ebitse <solidpaska@gmail.com>
2025-05-21 11:01:36 +0100

    new calculator

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,8 @@
+number1 = 10
+number2 = 5
+sum = 10 + 5
+difference = 10 - 5
+product = 10 * 5
+print(f"Addition of {number1} and {number2} is {sum}")
+print(f"Subtraction of {number1} and {number2} is {difference}")
+print(f"Multiplication of {number1} and {number2} is {product}")
---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,5 @@
+principal = 1000 # $1000
+rate = 0.05 #5%
+time = 3 #3years
+Interest = principal * rate * time
+print(f"The simple interest is: {Interest}")
