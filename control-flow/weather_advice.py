3ee6e39ca4d8665af15b7e3ad7923d3854568ba6
Paschal Ebitse <solidpaska@gmail.com>
2025-05-31 17:45:55 +0100

    My first control flow program

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,13 +1,10 @@
-weather_1 = "sunny"
-weather_2 = "rainy"
-weather_3 = "cold"
 
-weather_condition = str(input("Whats the weather like today?(sunny/rainy/cold): "))
-if weather_condition == weather_1:
+weather = input("Whats the weather like today?(sunny/rainy/cold): ")
+if weather == "sunny":
    print("Wear a t-shirt and sunglasses.")
-elif weather_condition == weather_2:
+elif weather == "rainy":
    print("Don't forget your umbrella and a raincoat.")
-elif weather_condition == weather_3:
+elif weather == "cold":
    print("Make sure to wear a warm coat and a scarf.")
 
 else:
