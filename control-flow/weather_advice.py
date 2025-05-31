fe53f6aded1fc16b94cd4b6ac9546d56a2e76c07
Paschal Ebitse <solidpaska@gmail.com>
2025-05-31 18:11:20 +0100

    My first control flow statement

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,11 +1,13 @@
-
-weather = input("Whats the weather like today?(sunny/rainy/cold): ")
-if weather == "sunny":
-   print("Wear a t-shirt and sunglasses.")
-elif weather == "rainy":
-   print("Don't forget your umbrella and a raincoat.")
-elif weather == "cold":
-   print("Make sure to wear a warm coat and a scarf.")
+'sunny' == "Wear a t-shirt and sunglasses."
+'rainy' == "Don't forget your umbrella and a raincoat."
+'cold' == "Make sure to wear a warm coat and a scarf."
+weather = input("Whats's the weather like today? (sunny/rainy/cold): ")
+if weather == 'sunny':
+    print("Wear a t-shirt and sunglasses.")
+elif weather == 'rainy':
+    print("Don't forget your umbrella and a raincoat.")
+elif weather == 'cold':
+    print("Make sure to wear a warm coat and a scarf.")
 
 else:
-   print("Sorry, I don't have recommendations for this weather.")
+    print("Sorry, I don't have recommendation for this weather.")
