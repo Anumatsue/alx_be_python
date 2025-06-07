3b366286df4beb617995901a5f013f8e8127315b
Paschal Ebitse <solidpaska@gmail.com>
2025-06-07 23:27:06 +0100

    Shopping list commit

---
+++ fns_and_dsa/shopping_list_manager.py
@@ -0,0 +1,33 @@
+
+def display_menu():
+    print("SHopping List Manager")
+    print("1. Add Item")
+    print("2. Remove Item")
+    print("3. View List")
+    print("4. Exit")
+
+def main():
+    shopping_list = []
+    while True:
+        display_menu()
+        choice = input("Enter your choice: ")
+
+        if choice == '1':
+            item = input("Enter Item: ")
+            shopping_list.append(item)
+        elif choice == '2':
+            item = input("Enter Item to be removed: ")
+            if item in shopping_list:
+                shopping_list.remove(item)
+            else:
+                print("Item not found")
+        elif choice == '3':
+            for i, item in enumerate(shopping_list, 1):
+                print(f"{i}. {item} ")
+        elif choice == '4':
+            print("Goodbye!")
+            break
+        else:
+            print("Invalid choice")
+
+main()
