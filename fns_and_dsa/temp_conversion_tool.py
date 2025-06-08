97957c6cb98f6578a90fe89cd000e11352a6d0a7
Paschal Ebitse <solidpaska@gmail.com>
2025-06-08 23:00:16 +0100

    Final commit for temp converter

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -3,10 +3,10 @@ CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
 
 def convert_to_celsius(fahrenheit):
     global FAHRENHEIT_TO_CELSIUS_FACTOR
-    return float(fahrenheit * FAHRENHEIT_TO_CELSIUS_FACTOR)
+    return FAHRENHEIT_TO_CELSIUS_FACTOR * fahrenheit
 def convert_to_fahrenheit(celsius):
     global CELSIUS_TO_FAHRENHEIT_FACTOR
-    return float(celsius * CELSIUS_TO_FAHRENHEIT_FACTOR)
+    return CELSIUS_TO_FAHRENHEIT_FACTOR * celsius
 user_temp = float(input("Enter the temperature to convert: "))
 scale = input("Is this temperature in celsius or fahrenheit? (C/F): ")
 if scale == "C":
@@ -17,4 +17,3 @@ elif scale == "F":
     print(f"{user_temp}\u00B0F is {result}\u00B0C")
 else:
     print("Invalid input")
-    
