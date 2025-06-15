23cc1d5a2dfafc6537eb67d73135f9c3bbfc200c
Paschal Ebitse <solidpaska@gmail.com>
2025-06-15 16:50:05 +0100

    New Commit

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,23 @@
+class BankAccount:
+    def __init__(self, account_balance):
+        self.account_balance = account_balance
+        account_balance = 0
+
+    def deposit(self, amount):
+        self.account_balance += amount
+    
+    def withdraw(self, amount):
+        if self.account_balance > amount:
+            self.account_balance -= amount
+            return True, self.account_balance
+        else:
+            return False, print(f"Insufficient funds.")
+        
+    def display_balance(self):
+        return f"Current Balance: ${self.account_balance}"
+              
+        
+
+
+
+            
