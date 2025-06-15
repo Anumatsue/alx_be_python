fb0a18f03f1951dee3fa71e4a1cf16746dd2ab25
Paschal Ebitse <solidpaska@gmail.com>
2025-06-15 22:56:00 +0100

    New unit test commit

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -9,7 +9,7 @@ class TestSimpleCalculator(unittest.TestCase):
         self.assertEqual(self.calc.add(2, -3), -1)
         self.assertEqual(self.calc.add(-2, 3), 1)
         self.assertEqual(self.calc.add(-2, -3), -5)
-    def test_subtract(self):
+    def test_subtraction(self):
         self.assertEqual(self.calc.subtract(10, 5), 5)
         self.assertEqual(self.calc.subtract(-10, 5), -15)
         self.assertEqual(self.calc.subtract(-10, -5), -5)
